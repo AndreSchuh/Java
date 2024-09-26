9e016341d791b44f5430cc1ca9818f6b66ec6d7a
Elieser Prichua <elieser.prichua@hypera.com.br>
2024-09-26 20:47:04 -0300

    - Mudei o método que gera os números para retornar números fixos para ficar mais fácil de comparar. - Comentei os filtros para ficar mais fácil de ver os exemplos. - Mudei para uma estrutura de List, para os números ficarem ordenados e ficar mais fácil de acompanhar.

--- src/main/java/br/andre/estudos/streams/StreamsBigDecimalExemplo.java
+++ src/main/java/br/andre/estudos/streams/StreamsBigDecimalExemplo.java
@@ -2,13 +2,15 @@ package br.andre.estudos.streams;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
 public class StreamsBigDecimalExemplo {
     public static void main(String[] args) {
-        Set<BigDecimal> numeros = geraSetComBigDecimal(10);
+        List<BigDecimal> numeros = geraSetComBigDecimal(10);
         imprimeSetComNumeros(numeros);
         calculaSoma(numeros);
         imprimeNumerosAcima50(numeros);
@@ -19,16 +21,36 @@ public class StreamsBigDecimalExemplo {
         imprimeNumeros2Quartil(numeros);
     }
 
-    private static Set<BigDecimal> geraSetComBigDecimal(int limit) {
+    private static List<BigDecimal> geraSetComBigDecimal(int limit) {
         Set<BigDecimal> numeros = new HashSet<>();
         Random random = new Random();
         for(int i = 0; i< limit; i++) {
             numeros.add(BigDecimal.valueOf(random.nextDouble(100)).setScale(2, RoundingMode.HALF_UP));
         }
-        return numeros;
+//        return numeros;
+        List<BigDecimal> nums = List.of(BigDecimal.valueOf(10),
+                BigDecimal.valueOf(15),
+                BigDecimal.valueOf(20),
+                BigDecimal.valueOf(25),
+                BigDecimal.valueOf(30),
+                BigDecimal.valueOf(35),
+                BigDecimal.valueOf(40),
+                BigDecimal.valueOf(45),
+                BigDecimal.valueOf(50),
+                BigDecimal.valueOf(55),
+                BigDecimal.valueOf(60),
+                BigDecimal.valueOf(65),
+                BigDecimal.valueOf(70),
+                BigDecimal.valueOf(75),
+                BigDecimal.valueOf(80),
+                BigDecimal.valueOf(85),
+                BigDecimal.valueOf(90),
+                BigDecimal.valueOf(95),
+                BigDecimal.valueOf(100));
+        return new ArrayList<>(nums);
     }
 
-    private static void imprimeSetComNumeros(Set<BigDecimal> numeros){
+    private static void imprimeSetComNumeros(List<BigDecimal> numeros){
         System.out.println("-----------------------------------");
         System.out.println("Imprimindo set com números");
 
@@ -36,7 +58,7 @@ public class StreamsBigDecimalExemplo {
 
     }
 
-    private static void calculaSoma(Set<BigDecimal> numeros){
+    private static void calculaSoma(List<BigDecimal> numeros){
         System.out.println("-----------------------------------");
         System.out.println("A soma dos números é: ");
         BigDecimal soma = numeros.stream()
@@ -44,7 +66,7 @@ public class StreamsBigDecimalExemplo {
         System.out.println(soma);
     }
 
-    private static void imprimeNumerosAcima50(Set<BigDecimal> numeros){
+    private static void imprimeNumerosAcima50(List<BigDecimal> numeros){
         System.out.println("-----------------------------------");
         System.out.println("Números acima de 50 são: ");
 
@@ -53,37 +75,37 @@ public class StreamsBigDecimalExemplo {
                 .forEach(System.out::println); // imprime cada número
     }
 
-    private static void numeros0a50comDesconto20PorCento(Set<BigDecimal> numeros){
+    private static void numeros0a50comDesconto20PorCento(List<BigDecimal> numeros){
         System.out.println("-----------------------------------");
         System.out.println("Números 0 a 50 com 20% de desconto: ");
 
         numeros.stream()
                 .filter(n -> n.compareTo(BigDecimal.valueOf(50)) <= 0) // Faz o "if else" de número entre 0 e 50
-                .map(n -> n.multiply(BigDecimal.valueOf(0.8))) // Aplica a cada elemento passado pelo filtro o desconto multiplicando por 0.8
+//                .map(n -> n.multiply(BigDecimal.valueOf(0.8))) // Aplica a cada elemento passado pelo filtro o desconto multiplicando por 0.8
                 .forEach(System.out::println);
     }
 
-    private static void numeros30a60comDesconto30PorCento(Set<BigDecimal> numeros){
+    private static void numeros30a60comDesconto30PorCento(List<BigDecimal> numeros){
         System.out.println("-----------------------------------");
         System.out.println("Números 30 a 60 com 30% de desconto: ");
 
         numeros.stream()
                 .filter(n -> n.compareTo(BigDecimal.valueOf(60)) <= 30)
-                .map(n -> n.multiply(BigDecimal.valueOf(0.7)))
+//                .map(n -> n.multiply(BigDecimal.valueOf(0.7)))
                 .forEach(System.out::println);
     }
 
-    private static void numeros50a100comDesconto35PorCento(Set<BigDecimal> numeros){
+    private static void numeros50a100comDesconto35PorCento(List<BigDecimal> numeros){
         System.out.println("-----------------------------------");
         System.out.println("Números 50 a 100 com 35% de desconto: ");
 
         numeros.stream()
                 .filter(n -> n.compareTo(BigDecimal.valueOf(100)) <= 50)
-                .map(n -> n.multiply(BigDecimal.valueOf(0.65)))
+//                .map(n -> n.multiply(BigDecimal.valueOf(0.65)))
                 .forEach(System.out::println);
     }
 
-    private static void somaNumeros1QuartilE3Quartil(Set<BigDecimal> numeros){
+    private static void somaNumeros1QuartilE3Quartil(List<BigDecimal> numeros){
         System.out.println("-----------------------------------");
         System.out.println("Soma dos números do 1º e 3º quartis: ");
 
@@ -99,7 +121,7 @@ public class StreamsBigDecimalExemplo {
         System.out.println(somaTotal);
     }
 
-    private static void imprimeNumeros2Quartil(Set<BigDecimal> numeros){
+    private static void imprimeNumeros2Quartil(List<BigDecimal> numeros){
         System.out.println("-----------------------------------");
         System.out.println("Imprime os números do 2º quartil: ");
 
