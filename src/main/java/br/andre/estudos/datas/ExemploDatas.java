97954fdb1e71f8959448bff52f8fab70d87c00ba
Elieser Prichua <elieser.prichua@hypera.com.br>
2024-10-30 10:15:48 -0300

    Exemplos de Datas.

---
+++ src/main/java/br/andre/estudos/datas/ExemploDatas.java
@@ -0,0 +1,20 @@
+package br.andre.estudos.datas;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class ExemploDatas {
+    public static void main(String[] args) {
+        LocalDate hoje = LocalDate.now();
+        System.out.println(hoje);
+
+        LocalDateTime agora = LocalDateTime.now();
+        System.out.println(agora);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        System.out.println(formatter.format(agora));
+
+
+    }
+}
