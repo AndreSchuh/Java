fc942eeb5ff64d175ebd0eba6279b0cd72a53276
André <andregabschuh@gmail.com>
2024-11-24 21:35:10 -0300

    Enviando exxercícios de Regex e seus devidos testes

--- src/main/java/br/andre/estudos/regex/RegexExercicio.java
+++ src/main/java/br/andre/estudos/regex/RegexExercicio.java
@@ -6,8 +6,6 @@ import java.util.regex.Pattern;
 
 public class RegexExercicio {
 
-    //O email e o cpf funcionam porém o data e o cnpj não
-
     private static final String EMAIL_REGEX = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
             + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
@@ -15,9 +13,9 @@ public class RegexExercicio {
 
     private static final String CNPJ_REGEX = "^[0-9]{2}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[\\/]?[0-9]{4}[-]?[0-9]{2}$";
 
-    private static final String DATA_REGEX = "/^(((0[1-9]|[12]\\d|3[01])\\/(0[13578]|1[02])\\/((19|[2-9]\\d)\\d{2}))|((0[1-9]|[12]\\d|30)\\/(0[13456789]|1[012])\\/((19|[2-9]\\d)\\d{2}))|((0[1-9]|1\\d|2[0-8])\\/02\\/((19|[2-9]\\d)\\d{2}))|(29\\/02\\/((1[6-9]|[2-9]\\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$/g";
+    private static final String DATA_REGEX = "^([0-2][0-9]||3[0-1])/(0[0-9]||1[0-2])/([0-9][0-9])?[0-9][0-9]$";
 
-    private static final String NUMEROTELEFONE_REGEX = "";
+    private static final String NUMEROTELEFONE_REGEX = "^([(]?[0]?[1-9]{2}[)]?)[9]?([1-9]{4})-?([0-9]{4})$";
 
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
@@ -41,8 +39,8 @@ public class RegexExercicio {
                 case 1:
                     System.out.print("Digite o email: ");
                     email = scanner.nextLine();
-                    validaEmail(email);
-                    if(validaEmail(email) == true){
+                    validaEmailRegex(email);
+                    if(validaEmailRegex(email) == true){
                         System.out.printf("O email %s é válido\n", email);
                     }else{
                         System.out.printf("O email digitado é inválido\n");
@@ -51,8 +49,8 @@ public class RegexExercicio {
                 case 2:
                     System.out.print("Digite o CPF: ");
                     cpf = scanner.nextLine();
-                    validaCpf(cpf);
-                    if(validaCpf(cpf) == true){
+                    validaCpfRegex(cpf);
+                    if(validaCpfRegex(cpf) == true){
                         System.out.printf("O cpf %s é válido\n", cpf);
                     }else{
                         System.out.printf("O cpf digitado é inválido\n");
@@ -61,8 +59,8 @@ public class RegexExercicio {
                 case 3:
                     System.out.print("Digite o CNPJ: ");
                     cnpj = scanner.nextLine();
-                    validaCnpj(cnpj);
-                    if(validaCnpj(cnpj) == true){
+                    validaCnpjRegex(cnpj);
+                    if(validaCnpjRegex(cnpj) == true){
                         System.out.printf("O cnpj %s é válido\n", cnpj);
                     }else{
                         System.out.printf("O cnpj digitado é inválido\n");
@@ -71,8 +69,8 @@ public class RegexExercicio {
                 case 4:
                     System.out.print("Digite a data: ");
                     data = scanner.nextLine();
-                    validaData(data);
-                    if(validaData(data) == true){
+                    validaDataRegex(data);
+                    if(validaDataRegex(data) == true){
                         System.out.printf("A data %s é válido\n", data);
                     }else{
                         System.out.printf("A data digitada é inválido\n");
@@ -81,8 +79,8 @@ public class RegexExercicio {
                 case 5:
                     System.out.print("Digite o número de telefone: ");
                     numeroTelefone = scanner.nextLine();
-                    validaEmail(numeroTelefone);
-                    if(validaEmail(numeroTelefone) == true){
+                    validaNumeroTelefoneRegex(numeroTelefone);
+                    if(validaNumeroTelefoneRegex(numeroTelefone) == true){
                         System.out.printf("O número de telefone %s é válido\n", numeroTelefone);
                     }else{
                         System.out.printf("O número de telefone é inválido\n");
@@ -95,31 +93,31 @@ public class RegexExercicio {
         }while(true);
     }
 
-    static boolean validaEmail(String email){
+    static boolean validaEmailRegex(String email){
         Pattern pattern = Pattern.compile(EMAIL_REGEX);
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
     }
 
-    static boolean validaCpf(String cpf){
+    static boolean validaCpfRegex(String cpf){
         Pattern pattern = Pattern.compile(CPF_REGEX);
         Matcher matcher = pattern.matcher(cpf);
         return matcher.matches();
     }
 
-    static boolean validaCnpj(String cnpj){
+    static boolean validaCnpjRegex(String cnpj){
         Pattern pattern = Pattern.compile(CNPJ_REGEX);
         Matcher matcher = pattern.matcher(cnpj);
         return matcher.matches();
     }
 
-    static boolean validaData(String data){
+    static boolean validaDataRegex(String data){
         Pattern pattern = Pattern.compile(DATA_REGEX);
         Matcher matcher = pattern.matcher(data);
         return matcher.matches();
     }
 
-    static boolean validaNumeroTelefone(String numeroTelefone){
+    static boolean validaNumeroTelefoneRegex(String numeroTelefone){
         Pattern pattern = Pattern.compile(NUMEROTELEFONE_REGEX);
         Matcher matcher = pattern.matcher(numeroTelefone);
         return matcher.matches();
